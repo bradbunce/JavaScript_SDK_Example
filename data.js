8e3c7b14eba2641125cfa4c11fce381708cb2cac
Brad Bunce <76120543+bradbunce@users.noreply.github.com>
2024-01-30 10:57:41 -0500

    Initial commit

---
+++ data.js
@@ -0,0 +1,13 @@
+var clientID = "CLIENT SDK KEY";
+var FeatureFlag = "FLAG KEY";
+var user = {
+    "key": "USER KEY",
+    "name": "USERNAME",
+    "firstName": "USER FIRSTNAME",
+    "lastName": "USER LASTNAME",
+    "email": "USER EMAIL ADDRESS",
+    "custom": {
+      "company": "USER COMPANY",
+      "group": "USER GROUP"
+    }
+  };
